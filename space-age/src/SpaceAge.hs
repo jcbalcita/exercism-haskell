3cdcd0b939a53759286aa1a5ebd65a4a0983ef63
jbalcita <jbalcita@cj.com>
2017-07-03 14:27:27 -0700

    haskell-ize indentations

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -16,12 +16,12 @@ secsToYears :: Planet -> Float -> Float
 secsToYears p m = do
   let earthYears = m / 60 / 60 / 24 / 365.25
   case p of
-    Mercury -> earthYears / 0.2408467
-    Venus   -> earthYears / 0.61519726
+    Mercury -> earthYears /   0.2408467
+    Venus   -> earthYears /   0.61519726
     Earth   -> earthYears
-    Mars    -> earthYears / 1.8808158
-    Jupiter -> earthYears / 11.862615
-    Saturn  -> earthYears / 29.447498
-    Uranus  -> earthYears / 84.016846
+    Mars    -> earthYears /   1.8808158
+    Jupiter -> earthYears /  11.862615
+    Saturn  -> earthYears /  29.447498
+    Uranus  -> earthYears /  84.016846
     Neptune -> earthYears / 164.79132
     _       -> error "Planet not found."
